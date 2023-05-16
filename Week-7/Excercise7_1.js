e22bbd304888905c8c79e013eaa436b7b2c2a6f0
Edris <edrisitprofessional@gmail.com>
2023-05-16 23:28:57 +0530

    Minor improvements

--- Week-7/Excercise7_1.js
+++ Week-7/Excercise7_1.js
@@ -1,21 +1,14 @@
 function getSubArrayWithMaxSum(arr) {
     let maxSum = 0;
     for (let i = 0; i < arr.length; i++) {
+        let subTotal = 0;
         for (let j = i; j < arr.length; j++) {
-            let subTotal = getSumOfSubArray(arr, i, j);
+            subTotal += arr[j];
             maxSum = subTotal > maxSum ? subTotal : maxSum;
         }
     }
 
     console.log(`Max Sum is: ${maxSum}`);
 }
-function getSumOfSubArray(arr, startingIndex, endingIndex) {
-    let subTotal = 0;
-    for (let index = startingIndex; index < endingIndex; index++) {
-        subTotal += arr[index];
-    }
 
-    return subTotal;
-}
-
-getSubArrayWithMaxSum([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
+getSubArrayWithMaxSum([-2, 1, -3, 4, 8, -1, 2, 1, -5, 4]);
